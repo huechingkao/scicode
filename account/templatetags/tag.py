346cd52d800fec29c743d60ec48e862d5e3a44ef
huechingkao <huechingkao@gmail.com>
2018-09-27 02:31:48 -0400

    first commit

---
+++ account/templatetags/tag.py
@@ -0,0 +1,36 @@
+from django import template
+from django.contrib.auth.models import User, Group
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from teacher.models import Classroom
+from account.models import MessagePoll
+
+register = template.Library()
+
+@register.filter
+def teacher_group(user):
+    return user.groups.filter(name='teacher').exists()
+  
+@register.filter
+def teacher_classroom(user_id, classroom_id):
+    classroom = Classroom.object.get(id=classroom_id)
+    if classroom.teacher_id == user_id:
+        return True
+    else:
+        return False
+
+@register.filter(takes_context=True)
+def realname(user_id):
+    try: 
+        user = User.objects.get(id=user_id)
+        return user.first_name
+    except :
+        pass
+    return ""
+
+@register.filter(takes_context=True)
+def read_already(message_id, user_id):
+    try:
+        messagepoll = MessagePoll.objects.get(message_id=message_id, reader_id=user_id)
+    except ObjectDoesNotExist:
+        messagepoll = MessagePoll()
+    return messagepoll.read    
